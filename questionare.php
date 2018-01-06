4183f90ffd5e7dc796f148f7c7771efa47bad4e5
oubdw678 <wanjak.buddeewong@hotmail.com>
2018-01-07 02:33:16 +0700

    Add PHP file

---
+++ questionare.php
@@ -0,0 +1,6 @@
+<?php
+$classPercentText = "per-txt-hide per-txt-100 per-txt-300";
+$BarSpeedJS = "fts stf linear";
+$BarSize = "progress-small progress-large";
+$BarStyle = "progress-bar-striped active";
+?>
